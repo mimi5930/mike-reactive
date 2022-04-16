1d60f673c9407f1b48138d1af410c87b644f13d4
Mike <mikej.miller440@gmail.com>
2022-04-15 20:22:24 -0500

    added footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
-  <section>Footer</section>;
+	return (
+		<section className="navbar fixed-bottom bg-dark text-white">
+			<div className="container-fluid">
+				<p>GitHub Link</p>
+				<p>Linked In Link</p>
+				<p>Stack Overflow Link</p>
+				<p>Twitter Link</p>
+			</div>
+		</section>
+	);
 };
 
 export default Footer;
