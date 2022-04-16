e80166448054debb8a97549fff4b1b6c9596e268
Mike <mikej.miller440@gmail.com>
2022-04-16 15:46:15 -0500

    specified footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<section className="navbar fixed-bottom bg-dark text-white">
+		<footer className="navbar fixed-bottom bg-dark text-white">
 			<div className="container-fluid">
 				<p>GitHub Link</p>
 				<p>Linked In Link</p>
 				<p>Stack Overflow Link</p>
 				<p>Twitter Link</p>
 			</div>
-		</section>
+		</footer>
 	);
 };
 
