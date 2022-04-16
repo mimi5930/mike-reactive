eabc348e89216524a3d3b4e2083c27e2adc8f58f
Mike <mikej.miller440@gmail.com>
2022-04-15 20:06:24 -0500

    added header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Header = () => {
-  return <section>Header</section>;
+	return (
+		<nav className="navbar bg-dark text-white">
+			<div className="container-fluid title">
+				<a className="navbar-brand text-white pt-0" href="/">
+					Mike Miller
+				</a>
+				<ul className="row justify-content-end">
+					<div className="nav-item col-auto">About Me</div>
+					<div className="nav-item col-auto">Portfolio</div>
+					<div className="nav-item col-auto">Resume</div>
+					<div className="nav-item col-auto">Contact</div>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
