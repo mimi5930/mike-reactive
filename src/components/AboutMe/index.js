edf6b1c5abb8be2614242b649f21b87a12639fe4
Mike <mikej.miller440@gmail.com>
2022-04-17 10:05:20 -0500

    edits to about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -3,18 +3,15 @@ import React from 'react';
 const AboutMe = () => {
 	return (
 		<section data-id="about-me">
-			<div>img</div>
+			<img
+				className="profile-img"
+				src={require('../../assets/img/mike.jpg')}
+			></img>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed nisi
-				sit amet enim porta congue. Donec tempor urna in eros suscipit, sed
-				tristique nibh tincidunt. Cras molestie porttitor justo nec consectetur.
-				Aenean ut rutrum ante, vitae egestas dui. Nullam rutrum dui quam, vitae
-				convallis tellus sagittis et. Maecenas lorem nisl, vehicula a enim quis,
-				commodo varius justo. Nunc vitae risus convallis, tempor tellus nec,
-				sollicitudin diam. Nullam purus orci, auctor ut auctor a, porta quis
-				turpis. Praesent at ipsum nec turpis porta facilisis id at lorem. Sed
-				congue nulla in quam consequat, at mattis magna viverra. Fusce id urna
-				pretium, rhoncus tortor et, tempus tellus.
+				Michael Miller is an aspiring web developer and musician in Woodbury,
+				Minnesota. Miller is currently enrolled in the University of Minnesota's
+				Coding Bootcamp through Trilogy Education Services where he is engaging
+				in front-end and back-end development.
 			</p>
 		</section>
 	);
