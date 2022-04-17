9429dc45514d4354f5405eeebed2a957bd918c61
Mike <mikej.miller440@gmail.com>
2022-04-17 10:36:14 -0500

    styled about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 const AboutMe = () => {
-	return (
-		<section data-id="about-me">
-			<img
-				className="profile-img"
-				src={require('../../assets/img/mike.jpg')}
-			></img>
-			<p>
-				Michael Miller is an aspiring web developer and musician in Woodbury,
-				Minnesota. Miller is currently enrolled in the University of Minnesota's
-				Coding Bootcamp through Trilogy Education Services where he is engaging
-				in front-end and back-end development.
-			</p>
-		</section>
-	);
+  return (
+    <section className="container-fluid d-flex flex-column m-3 align-items-center mt-5">
+      <img
+        alt="Mike smiling at the camera"
+        className="profile-img mb-5"
+        src={require('../../assets/img/mike.jpg')}
+      ></img>
+      <div className="card profile-text-container">
+        <p className="card-text profile-text">
+          Michael Miller is an aspiring web developer and musician in Woodbury,
+          Minnesota. Miller is currently enrolled in the University of
+          Minnesota's Coding Bootcamp through Trilogy Education Services where
+          he is engaging in front-end and back-end development.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default AboutMe;
